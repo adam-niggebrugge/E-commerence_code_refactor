fcbf28de7ca388f4f9e1a3cf34e694f578aed7a5
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-09-26 21:05:45 -0400

    initial trial of CRUD RESTful API

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -63,6 +63,12 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       }
     });
+
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No catagory with this id!' });
+      return;
+    }
+
     res.status(200).json(categoryData);
   }catch (err) {
     res.status(400).json(err);
