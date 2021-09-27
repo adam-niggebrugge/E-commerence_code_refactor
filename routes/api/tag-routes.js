17e72a9d8385bf1e42d3de07be9e52b56168951d
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-09-27 18:00:33 -0400

    fixed delete missed where clause

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -68,6 +68,8 @@ router.put('/:id', async (req, res) => {
     }
     res.status(200).json(tagData);
   } catch (err) {
+    console.log(`Inside put - err is - ${err}`);
+
     res.status(500).json(err);
   }
 });
@@ -75,7 +77,11 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Tag.destroy(req.body);
+    const tagData = await Tag.destroy({ 
+      where: {
+        id: req.params.id
+      }
+    });
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id!' });
       return;
